a91b0ed3179014666058aa134b0dceecfc1842a7
Prozid <daniprolongo@hotmail.com>
2012-05-24 23:43:50 +0200

    Añadido mapa para visualizar rutas.

---
+++ TortolasProject/Scripts/jsactions/Facturas/visorRutas.js
@@ -0,0 +1,48 @@
+﻿var map;
+$(document).ready(function () {
+    inicializarMapa();
+});
+function inicializarMapa() {
+
+    var myOptions = {
+        center: new google.maps.LatLng(-34.397, 150.644),
+        zoom: 8,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+    console.log("Mapa creado");
+    
+    $.ajax({
+        type: "GET",
+        url: "devolverRuta",
+        dataType: "xml",
+        success: function (xml) {
+            var points = [];
+            var bounds = new google.maps.LatLngBounds();
+            $(xml).find("trkpt").each(function () {
+                var lat = $(this).attr("lat");
+                var lon = $(this).attr("lon");
+                var p = new google.maps.LatLng(lat, lon);
+                points.push(p);
+                bounds.extend(p);
+            });
+
+            var poly = new google.maps.Polyline({
+                // use your own style here
+                path: points,
+                strokeColor: "#FF00AA",
+                strokeOpacity: .7,
+                strokeWeight: 4
+            });
+
+            poly.setMap(map);
+
+            // fit bounds to track
+            map.fitBounds(bounds);
+        }
+    });
+    
+   
+
+}
---
+++ TortolasProject/Views/Facturas/visorRuta.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Views/Shared/Site.Master" Inherits="System.Web.Mvc.ViewPage<dynamic>" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="TitleContent" runat="server">
+    visorRuta
+</asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+
+<h2>visorRuta</h2>
+<div id='map_canvas'></div>
+</asp:Content>
+
+<asp:Content ID="Content3" ContentPlaceHolderID="CssContent" runat="server">
+    <link href="../../Content/Facturas/visorRuta.css" rel="stylesheet" type="text/css" />
+</asp:Content>
+
+<asp:Content ID="Content4" ContentPlaceHolderID="ScriptContent" runat="server">
+    <script type="text/javascript"
+      src="http://maps.googleapis.com/maps/api/js?key=AIzaSyCc8eoHPdFfC_ZI0ncqfFxxwlxFGkDIQVs&sensor=true">
+    </script>
+    <script src="../../Scripts/jsactions/Facturas/visorRutas.js" type="text/javascript"></script>
+</asp:Content>
