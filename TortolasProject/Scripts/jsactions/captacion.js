c6324d35f21fc549d97eecc37ac7ff0ef8a876c9
fusrodah <ismikin@gmail.com>
2012-05-25 21:13:25 +0200

    Buff...

--- TortolasProject/Scripts/jsactions/captacion.js
+++ TortolasProject/Scripts/jsactions/captacion.js
@@ -8,9 +8,29 @@
 $(document).ready(function () {
 
     var estadisticas = {};
+    var descuentoBasico = null;
+    var descuentoAntiguo = null;
 
     $("#graficas").kendoTabStrip();
     $("#graficasSocios").kendoTabStrip();
+
+    $.ajax({
+        url: "../Socios/administrarDescuentosSocio",
+        type: "POST",
+        async: false,
+        success: function (descuentosRespuesta) {
+            descuentos = descuentosRespuesta;
+            if (descuentos[0].Nombre == "Basico") {
+                descuentoBasico = descuentos[0].Cantidad;
+                descuentoAntiguo = descuentos[1].Cantidad;
+            }
+            else {
+                descuentoBasico = descuentos[1].Cantidad;
+                descuentoAntiguo = descuentos[0].Cantidad;
+            }
+        }
+    });
+
 
     dataSourceAltas = new kendo.data.DataSource({
         transport: {
@@ -124,43 +144,36 @@ $(document).ready(function () {
         }
     });
 
-/*
-    $("#graficaEventos").kendoChart({
-        dataSource: dataSourceEventos,
-        title: {
-            text: "Estado de Socios actuales"
-        },
-        seriesDefaults: {
-            type: "pie",
-            labels: {
-                visible: true,
-                format: "{0}%"
-            }
-        },
-        legend: {
-            position: "bottom",
-            template: "#= nombre #"
-        },
-        series: [{
-            field: "porcentaje",
-            name: "nombre",
-            categoryField: "nombre"
-        },
-        {
-            field: "",
-            name: "nombre",
-            categoryField: "nombre"
-        }
-        ],
-        categoryAxis: {
-            field: "nombre"
-        },
-        tooltip: {
-            visible: true,
-            template: "${ category } <br> #= value# %"
+    $(".numeric").kendoNumericTextBox({
+        format: "c0",
+        min: 0
+    });
+
+    $("#comboTipoSocio").kendoDropDownList({
+        dataValueField: "valor",
+        dataTextField: "texto",
+        dataSource: [{ texto: "Basico", valor: "Basico" }, { texto: "Antiguo", valor: "Antiguo"}]
+    });
+
+    $("#generarResultado").click(function () {
+        var valorMaterial = $("#dineroMaterial").data("kendoNumericTextBox").value();
+        var valorEventos = $("#dineroEventos").data("kendoNumericTextBox").value();
+        var total = valorEventos + valorMaterial;
+        var resultado = 0;
+
+        var tipo = $("#comboTipoSocio").data("kendoDropDownList").value();
+
+        if (tipo == "Basico") {
+            
+            resultado =((total * descuentoBasico) / 100);
 
         }
-    });*/
-    
+        else {
+            resultado = ((total * descuentoAntiguo) / 100);
+        }
+
+        $("#resultado").html("Te ahorras "+resultado+" &euro;");
+    });
+
 });
        
